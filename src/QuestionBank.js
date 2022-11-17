a435ad5cc95b38659e1ce3708dc0796b75bef0f2
sandesh <sandeshdevadiga03@gmail.com>
2022-11-17 08:25:11 -0500

    change

--- src/QuestionBank.js
+++ src/QuestionBank.js
@@ -1,11 +1,11 @@
 const Questions=[
     {
-        promp:'What is 1+3 ?',
-        OptionA:'2',
-        OptionB:'5',
-        OptionC:'4',
-        OptionD:'6',
-        answer:'C'
+        promp:'Which year t20 is started ?',
+        OptionA:'2017',
+        OptionB:'2003',
+        OptionC:'2009',
+        OptionD:'2007',
+        answer:'D'
 
     },
     {
@@ -36,7 +36,66 @@ const Questions=[
         OptionD:'2009',
         answer:'C'
 
-    }
+    },
+    {
+        promp:'What year did the Titanic sink in the Atlantic Ocean on 15 April, on its maiden voyage from Southampton ?',
+        OptionA:'1912',
+        OptionB:'2007',
+        OptionC:'1863',
+        OptionD:'1563',
+        answer:'A'
+
+    },
+    {
+        promp:'What is the name of the biggest technology company in South Korea ?',
+        OptionA:'SAMSUNG',
+        OptionB:'REDMI',
+        OptionC:'NOKIA',
+        OptionD:'HP',
+        answer:'A'
+
+    },
+    {
+        promp:'Which year was the first Tonka truck made ?',
+        OptionA:'1626',
+        OptionB:'1947',
+        OptionC:'1946',
+        OptionD:'1856',
+        answer:'B'
+
+    },
+    {
+        promp:'In which year was The Godfather first released ?',
+        OptionA:'1986',
+        OptionB:'1957',
+        OptionC:'1946',
+        OptionD:'1972',
+        answer:'D'
+
+    },
+    {
+        promp:'What is the lifespan of a dragonfly ?',
+        OptionA:'24hr',
+        OptionB:'12hr',
+        OptionC:'45hr',
+        OptionD:'22hr',
+        answer:'A'
+
+    },
+    {
+        promp:'How many breaths does the human body take daily ?',
+        OptionA:'20000',
+        OptionB:'15000',
+        OptionC:'7000',
+        OptionD:'4562',
+        answer:'A'
+
+    },
+
+
+
+
+
 ]
 
 export default Questions
